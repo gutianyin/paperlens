9a2f85f5172ad8caefd854ef256cdf91b16b9252
xlvector@gmail.com <xlvector@gmail.com@b3d5c331-5047-8f5e-46d5-4841f003c595>
2012-01-02 06:33:34 +0000

    vim

--- trunk/site/reader/myfeed.php
+++ trunk/site/reader/myfeed.php
@@ -10,6 +10,16 @@ while($row = mysql_fetch_array($result))
 	$feeds .= $feed_id . ',';
 }
 $feeds .= '0';
+
+function PostProcess($buf)
+{
+	$p0 = stripos($buf, "</description>");
+	if($p0 == false) return $buf;
+	return substr($buf, 0, $p0) 
+	. htmlspecialchars("<p><a href=\"http://www.reculike.com/site/reader/\" target=_blank>RecULike : 个性化RSS选择工具</a></p>")
+	. substr($buf, $p0, strlen($buf));
+}
+
 ?>
 
 <rss version="2.0">
@@ -19,13 +29,13 @@ $feeds .= '0';
     <link>http://www.reculike.com/site/reader/</link>
 
 <?php
-$result = mysql_query("select a.article_id from feed_articles a, articles b where a.article_id = b.id and a.feed_id in ($feeds) order by b.pub_at desc limit 100");
+$result = mysql_query("select a.article_id from feed_articles a, articles b where a.article_id = b.id and a.feed_id in ($feeds) order by b.pub_at desc limit 20");
 while($row = mysql_fetch_array($result))
 {
 	$article_id = $row[0];
 	$itemxml = file_get_contents("http://www.reculike.com/site/reader/articles/" . (string)($article_id % 10) . "/" . (string)($article_id));
-	if(strlen($itemxml) > 500)
-		echo $itemxml;
+	#if(strlen($itemxml) > 300)
+	echo PostProcess($itemxml);
 }
 ?>
 </channel></rss>
